f7014507a331b1cd714cba634536048e9f6c281b
Emy2024 <emmanuelle.rey@outlook.com>
2024-12-13 12:00:18 +0100

    Ajout de la fonction main (le point d'entrée dans mon fichier JS)

--- assets/js/mesprojets.js
+++ assets/js/mesprojets.js
@@ -1,21 +1,29 @@
-let BTN_TOUSLESITEMS = document.createElement("button")
-let BTN_OBJETS = document.createElement("button")
-let BTN_APPARTEMENTS = document.createElement("button")
-let BTN_HOTELS_RESTAURANTS = document.createElement("button")
+const URL_API = "http://localhost:5678/api/works" 
+const BTN_TOUSLESITEMS = document.createElement("button")
+const BTN_OBJETS = document.createElement("button")
+const BTN_APPARTEMENTS = document.createElement("button")
+const BTN_HOTELS_RESTAURANTS = document.createElement("button")
 
+//Mon point d'entrée
+async function main(){
+  let data = await fetchDataAPI(URL_API) 
+  await displayGallery(data)
+  await displayFilter(data)
+  await initEvent(data)
+}
+main()
 
-async function fetchDataAPI() {
+async function fetchDataAPI(url) {
   try {
-    const response = await fetch("http://localhost:5678/api/works");
+    const response = await fetch(url);
     if (!response.ok) {
       throw new Error(`HTTP error! status: ${response.status}`);
     }
-    const DATAAPI = await response.json();
-    return DATAAPI; // Retourne les données pour qu'une autre fonction puisse les utiliser
+    return await response.json();
   } 
   catch (error) {
     console.error("Erreur lors du fetch :", error.message);
-    return []; // Retourne un tableau vide en cas d'erreur
+    return []; 
   }
 }
 
@@ -40,14 +48,11 @@ async function displayGallery(data){
 }  
 
 
-async function displayFilter(){
-  let data = await fetchDataAPI(); 
-
+async function displayFilter(data){
   let dataUniques = [... new Set(data.map(item => item.category.name))]
   const filtreObjet = dataUniques[0]
   const filtreAppartement = dataUniques[1]
   const filtreHotelEtRestaurant = dataUniques[2]
-  //console.log(dataUniques[1])
 
   let filterElement = document.querySelector(".filter")
 
@@ -64,38 +69,29 @@ async function displayFilter(){
   filterElement.appendChild(BTN_APPARTEMENTS)
   filterElement.appendChild(BTN_HOTELS_RESTAURANTS)
 }
-displayFilter()
 
 
+async function filter(data, filterName){
+    displayGallery(data.filter(obj => obj.category.name ===filterName))
+}
 
-async function initEvent(){
-  let data = await fetchDataAPI(); 
-  console.log(data)
 
-  let dataObj = data.filter(obj => obj.category.name ==="Objets" )
-  //console.log(dataObj)
-  let dataApp = data.filter(app => app.category.name ==="Appartements" )
-  //console.log(dataApp)
-  let dataHotelRestau = data.filter(hotelRestau => hotelRestau.category.name ==="Hotels & restaurants" )
-  //console.log(dataHotelRestau)
-
-  displayGallery(data)
-
+async function initEvent(data){
   BTN_TOUSLESITEMS.addEventListener("click", function() {
-    displayGallery(data)
+    displayGallery(data) 
   })
 
   BTN_OBJETS.addEventListener("click", function() {
-    displayGallery(dataObj)
+    filter(data, "Objets")
   })
 
   BTN_APPARTEMENTS.addEventListener("click", function() {
-    displayGallery(dataApp)
+    filter(data, "Appartements")
   })
 
   BTN_HOTELS_RESTAURANTS.addEventListener("click", function() {
-    displayGallery(dataHotelRestau)
+    filter(data, "Hotels & restaurants")
   })
 }
 
-initEvent()
+
