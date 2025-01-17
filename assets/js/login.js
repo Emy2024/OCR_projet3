5943d19d776365e20c029b51f49717c8aaf3370f
Emy2024 <emmanuelle.rey@outlook.com>
2025-01-17 09:59:11 +0100

    #108 - Suppression du token visible.Le token est automatiquement généré grâce au Backend

--- assets/js/login.js
+++ assets/js/login.js
@@ -106,7 +106,6 @@ async function postData() {
     method: "POST",
     headers: {
       'Content-Type': 'application/json',
-      'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImlhdCI6MTY1MTg3NDkzOSwiZXhwIjoxNjUxOTYxMzM5fQ.JGN1p8YIfR-M-5eQ-Ypy6Ima5cKA4VbfL2xMr2MgHm4',
     },
     body: payload, 
   })
