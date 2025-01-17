b6ce45a655c3699c5d8bb9f476b67c5d71eee4c9
Emy2024 <emmanuelle.rey@outlook.com>
2025-01-17 10:00:32 +0100

    #48 - Dans le headers du fetch Delete, ajout de l'authorisation Bearer ainsi que du token présent en localStorage

--- assets/js/fetchPortfolio.js
+++ assets/js/fetchPortfolio.js
@@ -41,11 +41,11 @@ export async function fetchCategoryAPI() {
 
 
 // Fetch delete pour supprimer les images présentes dans l'API, partie createGalleryModale()
-export async function fetchDelete(id, imgElement) {
+export async function imageDelete(id, imgElement) {
   fetch (`http://localhost:5678/api/works/${id}`, {
     method: "DELETE",
      headers: {
-      'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImlhdCI6MTY1MTg3NDkzOSwiZXhwIjoxNjUxOTYxMzM5fQ.JGN1p8YIfR-M-5eQ-Ypy6Ima5cKA4VbfL2xMr2MgHm4',
+      'Authorization': 'Bearer '+ localStorage.getItem('token'),
     } 
   })
   .then(response => {
